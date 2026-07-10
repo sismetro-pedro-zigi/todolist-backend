08d7edb3f7067cf4460135b9a2765d58f048415c
sismetro-pedro-zigi <pedro.zigiotto@sismetro.com>
2026-07-10 16:03:41 -0300

    feat: connecting everything

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const cors = require('cors');
+const taskRoutes = require('./src/routes/task.routes');
 
 const app = express();
 const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/tasks", taskRoutes);
 
 app.get("/", (req, res) => {
     res.send("API está funcional");
